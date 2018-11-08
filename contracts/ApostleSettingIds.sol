a8dc86216ffa051fa085cc0eaf89a7a91028989a
董淑伟 <dsw0602@foxmail.com>
2018-11-08 17:41:52 +0800

    initialize apostle contact

---
+++ contracts/ApostleSettingIds.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+import "@evolutionland/common/contracts/SettingIds.sol";
+
+
+contract ApostleSettingIds is SettingIds {
+
+    bytes32 public constant CONTRACT_GENE_SCIENCE = "CONTRACT_GENE_SCIENCE";
+}
