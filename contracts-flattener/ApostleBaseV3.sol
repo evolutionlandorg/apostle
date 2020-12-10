bce063b9767e527336aa87370bb54f1bf70996ee
echo <hujw77@gmail.com>
2020-12-10 19:45:24 +0800

    fix

--- contracts-flattener/ApostleBaseV3.sol
+++ contracts-flattener/ApostleBaseV3.sol
@@ -1332,7 +1332,7 @@ contract ApostleBaseV3 is ApostleBaseV2 {
     using SafeMath for uint256;
 	
 	// 0x434f4e54524143545f41504f53544c455f4954454d5f42415200000000000000
-	bytes32 public constant CONTRACT_LAND_ITEM_BAR = "CONTRACT_APOSTLE_ITEM_BAR";
+	bytes32 public constant CONTRACT_APOSTLE_ITEM_BAR = "CONTRACT_APOSTLE_ITEM_BAR";
 
 	// rate precision
 	uint112 public constant RATE_DECIMALS = 10**8;
@@ -1341,8 +1341,8 @@ contract ApostleBaseV3 is ApostleBaseV2 {
         uint talents = tokenId2Apostle[_tokenId].talents;
         uint256 strength = IGeneScience(registry.addressOf(CONTRACT_GENE_SCIENCE))
         .getStrength(talents, _resourceToken, _landTokenId);
-
-		address itemBar = registry.addressOf(CONTRACT_LAND_ITEM_BAR);
+		// V3
+		address itemBar = registry.addressOf(CONTRACT_APOSTLE_ITEM_BAR);
 		uint256 enhanceRate =
 			IItemBar(itemBar).enhanceStrengthRateOf(_resourceToken, _tokenId);
 		uint256 enhanceStrength = strength.mul(enhanceRate).div(RATE_DECIMALS);
