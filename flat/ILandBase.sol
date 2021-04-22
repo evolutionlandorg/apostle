594d5efdd89e9125896e98a1c329d8c5ef3a49be
echo <hujw77@gmail.com>
2021-04-22 19:37:04 +0800

    dapp compatible

--- flat/ILandBase.sol
+++
@@ -1,8 +0,0 @@
-// Root file: contracts/interfaces/ILandBase.sol
-
-pragma solidity ^0.4.24;
-
-contract ILandBase {
-
-    function resourceToken2RateAttrId(address _resourceToken) public view returns (uint256);
-}
