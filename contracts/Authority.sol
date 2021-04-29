73cd22ad911f852c935d4ec9bd8fdf239e8fc103
echo <hujw77@gmail.com>
2021-04-29 15:42:53 +0800

    upgrade authority

--- contracts/Authority.sol
+++ contracts/Authority.sol
@@ -11,22 +11,22 @@ contract Authority {
     mapping (address => uint) public wards;
     mapping (bytes4 => uint)  public sigs;
     modifier sudo                               { require(msg.sender == root); _; }
-    function setRoot(address usr) external sudo { root = usr; emit SetRoot(usr); }
-    function rely(address usr)    external sudo { wards[usr] = 1; emit Rely(usr); }
-    function deny(address usr)    external sudo { wards[usr] = 0; emit Deny(usr); }
-    function allow(bytes4 sig)    external sudo { sigs[sig] = 1; emit Allow(sig); }
-    function forbid(bytes4 sig)   external sudo { sigs[sig] = 1; emit Forbid(sig); }
+    function setRoot(address usr) public sudo { root = usr; emit SetRoot(usr); }
+    function rely(address usr)    public sudo { wards[usr] = 1; emit Rely(usr); }
+    function deny(address usr)    public sudo { wards[usr] = 0; emit Deny(usr); }
+    function allow(bytes4 sig)    public sudo { sigs[sig] = 1; emit Allow(sig); }
+    function forbid(bytes4 sig)   public sudo { sigs[sig] = 1; emit Forbid(sig); }
 
     constructor(address[] _wards, bytes4[] _sigs) public {
         root = msg.sender;
         emit SetRoot(root);
-        for (uint i = 0; i < _wards.length; i ++) { rely(_wards[i]); }
-        for (uint i = 0; i < _sigs.length; i++) { allow(_sigs[i]); }
+        for (uint i = 0; i < _wards.length; i++) { rely(_wards[i]); }
+        for (uint j = 0; j < _sigs.length; j++) { allow(_sigs[j]); }
     }
 
     function canCall(
         address _src, address, bytes4 _sig
     ) public view returns (bool) {
-        return ( wards[_src] == 1 && sigs[_sig] == 1) 
+        return wards[_src] == 1 && sigs[_sig] == 1;
     }
 }
